a1e0842a5bea2e3a4eb329bbd7ab93978090fabb
Oscar Pérez <mr.omiguelperez@gmail.com>
2018-09-09 22:38:16 -0500

    Saving and getting related data with Entity Framework Core

---
+++ FinancesDataAccess.TestConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace FinancesDataAccess.TestConsole
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BloggingContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
+            #region AddingGraphOfEntities
+            using (var context = new BloggingContext())
+            {
+                var blog = new Blog
+                {
+                    Url = "http://blogs.msdn.com/dotnet",
+                    Posts = new List<Post>
+                    {
+                        new Post { Title = "Intro to C#" },
+                        new Post { Title = "Intro to VB.NET" },
+                        new Post { Title = "Intro to F#" }
+                    }
+                };
+
+                context.Blogs.Add(blog);
+                context.SaveChanges();
+            }
+            #endregion
+
+            #region GettingRelatedData
+            using (var context = new BloggingContext())
+            {
+                var blogs = context.Blogs.Include(b => b.Posts);
+                foreach (var blog in blogs)
+                {
+                    Console.WriteLine($"{blog.BlogId} - {blog.Url}");
+                }
+                Console.ReadKey();
+            }
+            #endregion
+        }
+
+    }
+}
