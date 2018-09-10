41ebc4fd9a21ac42b25a46df390ae5201ee91f2c
Oscar Pérez <mr.omiguelperez@gmail.com>
2018-09-09 21:00:04 -0500

    Creating database from entities adding them to DB context

---
+++ DataAccess/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccess
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+    }
+}
---
+++ DataAccess/SchoolContext.cs
@@ -0,0 +1,15 @@
+﻿namespace DataAccess
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class SchoolContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
