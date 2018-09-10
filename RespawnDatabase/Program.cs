8f4db6aa97fdab728aee707316995d103df90e48
Oscar Pérez <mr.omiguelperez@gmail.com>
2018-09-10 07:43:01 -0500

    Reset data from all tables excluding Courses table

---
+++ RespawnDatabase/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace RespawnDatabase
+{
+    using Respawn;
+    using System;
+    using System.Data.SqlClient;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResetDatabase().Wait();
+        }
+
+        static async Task ResetDatabase()
+        {
+            Checkpoint checkpoint = new Checkpoint
+            {
+                TablesToIgnore = new[]
+                {
+                    "Courses"
+                },
+                DbAdapter = DbAdapter.SqlServer
+            };
+
+            string connectionString = @"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                await checkpoint.Reset(connection);
+            }
+        }
+    }
+}
